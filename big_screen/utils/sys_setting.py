1e7356c5bc40b337be7426db2c92343a5f679cae
yx <gybond1995@foxmail.com>
2021-07-27 16:50:35 +0800

    migrations

--- big_screen/utils/sys_setting.py
+++ big_screen/utils/sys_setting.py
@@ -37,7 +37,7 @@ NEIMENGGU = 150000
 YULINSHI = 610800
 wuhai = 150300
 # 当前所在以及行政区
-SYS_DISTRICT = CHONGQING
+SYS_DISTRICT = wuhai
 
 # 正则验证格式
 DATE_FORMATTER_RE = "[0-9]{4}-[0-9]{2}-[0-9]{2} [0-9]{2}:[0-9]{2}:[0-9]{2}"  # 时间
